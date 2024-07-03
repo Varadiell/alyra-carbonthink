a639b458192687af68754dbefeb4299bc0f9be82
William SIMON--VEZO <william.simonvezo@gmail.com>
2024-07-03 12:05:56 +0200

    TCO2 token royalties

--- backend/contracts/TCO2.sol
+++ backend/contracts/TCO2.sol
@@ -4,18 +4,22 @@ pragma solidity ^0.8.20;
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-error EmptyMetadata();
-error MetadataAlreadySet(uint256 tokenId);
+error MintAmountZero(); // Mint amount should not be zero.
+error MintEmptyMetadata(); // Mint metadata to set should not be empty (on token creation).
+error TokenMetadataExists(uint256 tokenId); // New metadata should not be set to an already existing token.
 
 /// @custom:security-contact security@carbonthink.xyz
-contract TCO2 is ERC1155, ERC1155Burnable, ERC1155Supply, Ownable {
+contract TCO2 is ERC1155, ERC1155Burnable, ERC1155Supply, ERC2981, Ownable {
     mapping(uint256 => string) private _metadatas;
 
     /// @notice Constructor to initialize the contract with an initial owner.
-    /// @dev No ERC1155 uri is set because we override the uri method.
-    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
+    /// @dev No ERC1155 uri is set because we override the uri function for on-chain metadata.
+    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {
+        _setDefaultRoyalty(initialOwner, 500); // Set royalties to 5%.
+    }
 
     /// @notice Mint a new token with the specified metadata.
     /// @dev Mints a specified amount of tokens of a given id to the specified address.
@@ -24,10 +28,16 @@ contract TCO2 is ERC1155, ERC1155Burnable, ERC1155Supply, Ownable {
     /// @param amount The amount of tokens to mint.
     /// @param base64Metadata The metadata associated with the token, encoded in base64.
     function mint(address account, uint256 id, uint256 amount, string memory base64Metadata) external onlyOwner {
-        // Set metadata before mint because of "exists" check.
-        if (!exists(id)) {
-            _setMetadata(id, base64Metadata);
+        if (amount == 0) {
+            revert MintAmountZero();
+        } else if (exists(id) && bytes(base64Metadata).length > 0) {
+            revert TokenMetadataExists(id);
+        } else if (!exists(id) && bytes(base64Metadata).length == 0) {
+            revert MintEmptyMetadata();
+        } else {
+            _metadatas[id] = base64Metadata;
         }
+        // Mint after "exists" checks.
         _mint(account, id, amount, "");
     }
 
@@ -49,7 +59,15 @@ contract TCO2 is ERC1155, ERC1155Burnable, ERC1155Supply, Ownable {
             );
     }
 
-    /// @notice Override the URI function to provide on-chain token-specific metadata.
+    /// @notice Overrides the supportsInterface function to include ERC2981 (royalty standard) support.
+    /// @dev This function overrides `supportsInterface` to handle the ERC2981 interface check.
+    /// @param interfaceId The interface identifier, as specified in ERC-165.
+    /// @return bool True if the contract implements the requested interface, false otherwise.
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, ERC2981) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /// @notice Overrides the URI function to provide on-chain token-specific metadata.
     /// @dev This function returns the metadata URI for a given token id.
     /// @param tokenId The token id to get the metadata URI for.
     /// @return string The metadata URI for the given token id.
@@ -59,17 +77,6 @@ contract TCO2 is ERC1155, ERC1155Burnable, ERC1155Supply, Ownable {
             return "";
         }
         return string.concat("data:application/json;base64,", jsonBase64);
-    }
-
-    /// @notice Internal function to set the json metadata for a token id when a new token is created.
-    /// @dev This function shall not be called on an already existing token.
-    /// @param id The token id to set the metadata for.
-    /// @param base64Metadata The metadata to set.
-    function _setMetadata(uint256 id, string memory base64Metadata) internal {
-        if (bytes(base64Metadata).length == 0) {
-            revert EmptyMetadata();
-        }
-        _metadatas[id] = base64Metadata;
     }
 
     /// @notice Override required by Solidity for token transfer updates.
