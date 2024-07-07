bb983cf3569b723938100f6d2a75164ae70236ad
William SIMON--VEZO <william.simonvezo@gmail.com>
2024-07-07 19:54:24 +0200

    weird vercel error

--- frontend/src/app/eventstco2/page.tsx
+++ frontend/src/app/eventstco2/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-import { Breadcrumbs } from '@/components/shared/breadcrumbs';
-import { EventsList } from '@/components/shared/events-list';
-import { DataContext } from '@/contexts/data-provider';
-import { useContext } from 'react';
+// import { Breadcrumbs } from '@/components/shared/breadcrumbs';
+// import { EventsList } from '@/components/shared/events-list';
+// import { DataContext } from '@/contexts/data-provider';
+// import { useContext } from 'react';
 
 export default function EventsTco2() {
-  const {
-    data: { tco2EventLogs },
-  } = useContext(DataContext);
+  // const {
+  //   data: { tco2EventLogs },
+  // } = useContext(DataContext);
 
   return (
     <>
-      <Breadcrumbs layers={['Home', 'Events TCO2']} />
-      <EventsList eventLogs={tco2EventLogs} />
+      {/* <Breadcrumbs layers={['Home', 'Events TCO2']} />
+      <EventsList eventLogs={tco2EventLogs} /> */}
     </>
   );
 }
--- frontend/src/components/shared/events-list.tsx
+++ frontend/src/components/shared/events-list.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { EventLog } from '@/contexts/data-provider';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
