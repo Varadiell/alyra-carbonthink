68008cde301bf16b3e6e6e90433da7b7982d5e00
William SIMON--VEZO <william.simonvezo@gmail.com>
2024-07-07 19:59:21 +0200

    revert

--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-// import { Breadcrumbs } from '@/components/shared/breadcrumbs';
-// import { EventsList } from '@/components/shared/events-list';
-// import { DataContext } from '@/contexts/data-provider';
-// import { useContext } from 'react';
+import { Breadcrumbs } from '@/components/shared/breadcrumbs';
+import { EventsList } from '@/components/shared/events-list';
+import { DataContext } from '@/contexts/data-provider';
+import { useContext } from 'react';
 
 export default function Events() {
-  // const {
-  //   data: { eventLogs },
-  // } = useContext(DataContext);
+  const {
+    data: { eventLogs },
+  } = useContext(DataContext);
 
   return (
     <>
-      {/* <Breadcrumbs layers={['Home', 'Events']} />
-      <EventsList eventLogs={eventLogs} /> */}
+      <Breadcrumbs layers={['Home', 'Events']} />
+      <EventsList eventLogs={eventLogs} />
     </>
   );
 }
