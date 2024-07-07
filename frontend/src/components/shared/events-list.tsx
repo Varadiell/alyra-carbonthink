73a6cf3b09512698f6990e4b56c13f2fc4bebf55
William SIMON--VEZO <william.simonvezo@gmail.com>
2024-07-07 14:57:14 +0200

    fixed events display

--- frontend/src/components/shared/events-list.tsx
+++ frontend/src/components/shared/events-list.tsx
@@ -2,7 +2,7 @@
 
 import { DataContext, EventLog } from '@/contexts/data-provider';
 import { useContext } from 'react';
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Badge } from '@/components/ui/badge';
@@ -45,64 +45,66 @@ export function EventsList() {
 
   return (
     <Card>
-      <CardHeader className="bg-muted/50">
+      <CardHeader className="bg-muted">
         <CardTitle>Events</CardTitle>
         <CardDescription>List of events that occurred.</CardDescription>
       </CardHeader>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Block number</TableHead>
-            <TableHead>Event name</TableHead>
-            <TableHead>Arguments</TableHead>
-            <TableHead>Transaction hash</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {!eventLogs
-            ? [...new Array(3)].map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell>
-                    <Skeleton className="h-4 w-1/2" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton className="h-4 w-1/2" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton className="h-4 w-1/2" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton className="h-4 w-1/2" />
-                  </TableCell>
-                </TableRow>
-              ))
-            : eventLogs?.map((event, index) => (
-                <TableRow key={index}>
-                  <TableCell>
-                    <a href={`https://basescan.org/block/${Number(event.blockNumber)}`} target="_blank">
-                      <Badge variant="secondary">{Number(event.blockNumber)}</Badge>
-                    </a>
-                  </TableCell>
-                  <TableCell>
-                    <Badge>{event.eventName}</Badge>
-                  </TableCell>
-                  <TableCell className="flex gap-2 flex-col">
-                    {Object.entries(parseEvent(event)).map(([k, v], index) => (
-                      <div key={index} className="flex gap-2">
-                        {k}
-                        <Badge variant="secondary">{v}</Badge>
-                      </div>
-                    ))}
-                  </TableCell>
-                  <TableCell>
-                    <a href={`https://basescan.org/tx/${Number(event.transactionHash)}`} target="_blank">
-                      <Badge variant="secondary">{event.transactionHash}</Badge>
-                    </a>
-                  </TableCell>
-                </TableRow>
-              ))}
-        </TableBody>
-      </Table>
+      <CardContent className="p-0 lg:w-[min(calc(100vw-330px),974px)] md:w-[calc(100vw-254px)] w-[max(calc(100vw-34px),393px)]">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="min-w-36">Block number</TableHead>
+              <TableHead>Event name</TableHead>
+              <TableHead>Arguments</TableHead>
+              <TableHead>Transaction hash</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {!eventLogs
+              ? [...new Array(3)].map((_, index) => (
+                  <TableRow key={index}>
+                    <TableCell>
+                      <Skeleton className="h-4 w-1/2" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-4 w-1/2" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-4 w-1/2" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-4 w-1/2" />
+                    </TableCell>
+                  </TableRow>
+                ))
+              : eventLogs?.map((event, index) => (
+                  <TableRow key={index}>
+                    <TableCell>
+                      <a href={`https://basescan.org/block/${Number(event.blockNumber)}`} target="_blank">
+                        <Badge variant="secondary">{Number(event.blockNumber)}</Badge>
+                      </a>
+                    </TableCell>
+                    <TableCell>
+                      <Badge>{event.eventName}</Badge>
+                    </TableCell>
+                    <TableCell className="flex gap-2 flex-col">
+                      {Object.entries(parseEvent(event)).map(([k, v], index) => (
+                        <div key={index} className="flex gap-2">
+                          {k}
+                          <Badge variant="secondary">{v}</Badge>
+                        </div>
+                      ))}
+                    </TableCell>
+                    <TableCell>
+                      <a href={`https://basescan.org/tx/${Number(event.transactionHash)}`} target="_blank">
+                        <Badge variant="secondary">{event.transactionHash}</Badge>
+                      </a>
+                    </TableCell>
+                  </TableRow>
+                ))}
+          </TableBody>
+        </Table>
+      </CardContent>
     </Card>
   );
 }
