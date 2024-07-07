a6ad7d4196cbeddf844657d7f6399a36609dff0c
William SIMON--VEZO <william.simonvezo@gmail.com>
2024-07-07 17:05:59 +0200

    useData reset on user change & chain change

--- frontend/src/hooks/useData.ts
+++ frontend/src/hooks/useData.ts
@@ -3,15 +3,26 @@
 import { useAccount, useWatchContractEvent } from 'wagmi';
 import { DataType, EventLog } from '@/contexts/data-provider';
 import { projectManagerContract } from '@/contracts/projectManager.contract';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useData(): DataType {
   const [eventLogs, setEventLogs] = useState<EventLog[] | undefined>(undefined);
 
-  const { isConnected, address } = useAccount();
+  const { isConnected, address, chain } = useAccount();
+
+  useEffect(() => {
+    // Chain change, reset all data.
+    setEventLogs(undefined);
+  }, [chain]);
+
+  useEffect(() => {
+    // User change, reset user data.
+    // TODO: reset user data
+  }, [address]);
 
   useWatchContractEvent({
     ...projectManagerContract,
+    chainId: chain?.id,
     onLogs: (logs) => setEventLogs(logs.reverse() as EventLog[]),
     enabled: isConnected,
   });
